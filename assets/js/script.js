740dcfe7c81f6c7570d5e3946287855f673c7ae9
tgrutomo <utomoputra70@gmail.com>
2024-11-02 11:42:21 -0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+// Script sederhana untuk mengelola aksi tombol
+function editKaryawan() {
+    alert('Edit karyawan dipilih');
+}
+
+function hapusKaryawan() {
+    if (confirm('Apakah Anda yakin ingin menghapus data ini?')) {
+        alert('Data karyawan dihapus');
+    }
+}
+
+function tambahKaryawan() {
+    alert('Form tambah karyawan akan ditampilkan');
+}
+
+function editJabatan() {
+    alert('Edit jabatan dipilih');
+}
+
+function hapusJabatan() {
+    if (confirm('Apakah Anda yakin ingin menghapus jabatan ini?')) {
+        alert('Jabatan dihapus');
+    }
+}
+
+function tambahJabatan() {
+    alert('Form tambah jabatan akan ditampilkan');
+}
+
+function editDivisi() {
+    alert('Edit divisi dipilih');
+}
+
+function hapusDivisi() {
+    if (confirm('Apakah Anda yakin ingin menghapus divisi ini?')) {
+        alert('Divisi dihapus');
+    }
+}
+
+function tambahDivisi() {
+    alert('Form tambah divisi akan ditampilkan');
+}
+
+function editAbsensi() {
+    alert('Edit absensi dipilih');
+}
+
+function hapusAbsensi() {
+    if (confirm('Apakah Anda yakin ingin menghapus absensi ini?')) {
+        alert('Absensi dihapus');
+    }
+}
+
+function tambahAbsensi() {
+    alert('Form tambah absensi akan ditampilkan');
+}
+
+function editIzin() {
+    alert('Edit izin dipilih');
+}
+
+function hapusIzin() {
+    if (confirm('Apakah Anda yakin ingin menghapus izin ini?')) {
+        alert('Izin dihapus');
+    }
+}
+
+function tambahIzin() {
+    alert('Form tambah izin akan ditampilkan');
+}
